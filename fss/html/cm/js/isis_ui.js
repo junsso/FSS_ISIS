32efe09fe6441eff194f0478afe1ad783921760b
junsso <junsso26@gmail.com>
2023-05-23 17:23:52 +0900

    수정

--- fss/html/cm/js/isis_ui.js
+++ fss/html/cm/js/isis_ui.js
@@ -8,19 +8,7 @@ $(document).ready(function(){
     $(toggleTitle).on('click',function(){
         $(this).parent(toggleBox).find(toggleConts).stop().slideToggle(300);
         $(this).parent(toggleBox).toggleClass('current');
-    });
-
-     //tab 스크립트
-     $('.tab-ui-js-menu .tab-ui-js-target').click(function(){
-        var tab_id = $(this).attr('data-tab');
-    
-        $('.tab-ui-js-menu .tab-ui-js-target').removeClass('current');
-        $('.tab-contents').removeClass('current');
-        
-        $(this).addClass('current');
-        $("#"+tab_id).addClass('current');
-        return false;
-    });
+    });     
 
     //ISIS자주찾는 업무 시스템
     var swiper = new Swiper(".is-fv-list .swiper", {
