8004cd33a19b006f5a4f7141ac3eca92e08bd45c
junsso <junsso26@gmail.com>
2023-06-14 16:25:40 +0900

    키워드찾기 완료

--- fss/html/cm/js/isis_ui.js
+++ fss/html/cm/js/isis_ui.js
@@ -28,7 +28,33 @@ $(document).ready(function(){
       
       $(this).addClass('current');
       return false;
+    });
+
+    /** 시스템 개요 조회 팝업 영역 **/
+    //tab
+    $('.is-tab-btn .is-multi-toggle').click(function(){
+      var tab_id = $(this).attr('data-tab');
+      var desc_id = $(this).attr('data-desc');
+      
+      $('.is-tab-btn .is-multi-toggle').removeClass('current');
+      $('.is-conts-contain').removeClass('current');
+      $('.is-desc').removeClass('current');
+      
+      $(this).addClass('current');
+      $("#"+tab_id).addClass('current');
+      $("#"+desc_id).addClass('current');
+      return false;
   });
+
+    //tree (시스템목록)
+    let tgTreeP = $('.is-tree li');
+    let tgTree = $('.is-tree > li > a');
+    let tgTreeUb = $('.is-subtree');
+    
+    $(tgTree).on('click',function(){
+        $(this).parent(tgTreeP).find(tgTreeUb).stop().slideToggle(300);
+        $(this).parent(tgTreeP).toggleClass('toggle');
+    }); 
 
 
 })
